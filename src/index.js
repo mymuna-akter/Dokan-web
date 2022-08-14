084e512a569fa4474e24cd74a3cdaf9df167f034
devded <devded@pm.me>
2022-08-14 07:04:18 +0000

    Add boostrap and fix footer to bottom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import reportWebVitals from './optional/reportWebVitals';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
